62d90e5eed754edf33b08b7f05f3f585bf8d0fd9
pimikeymike <mgar_datascience@protonmail.com>
2019-05-27 01:11:46 -0400

    updates

--- helper.R
+++ helper.R
@@ -1,31 +1,37 @@
-top_products <- function(department = NULL, aisle = NULL, topN = 10){
-  as.character(department)
-  as.character(aisle)
+top_companies <- function(physician_specialty = NULL,  topN = 10){
+  as.character(physician_specialty)
   
-  if(is.null(department) == FALSE & is.null(aisle) == FALSE)
+  if(is.null(physician_specialty) == FALSE)
   {
+    toppayor <- aggregate(Total_Amount_of_Payment_USDollars ~ Submitting_Applicable_Manufacturer_or_Applicable_GPO_Name
+                          + Physician_Specialty
+                          ,cmspayments,FUN = sum)
+    colnames(toppayor) <- c("Company","TotalPayment", "Physician_Specialty")
     
-    oppd <- aggregate(order_prod_prior_dept$order_id
-                      , list(order_prod_prior_dept$department
-                             ,order_prod_prior_dept$aisle
-                             ,order_prod_prior_dept$product_name)
-                      , length)
+    
     # used ddply but it gives same result
     #a <-  plyr::ddply(order_prod_prior_dept,.(department, aisle,product_name),summarize
     #             ,no_dist_orders=(length(unique(order_id))))
-    names(oppd) <- c("department", "aisle", "product_name", "no_orders")
-    das <- oppd[which(oppd$department == department & oppd$aisle == aisle),]
+    names(toppayor) <- c("department", "aisle", "product_name", "no_orders")
+    das <- toppayor[which(toppayor$department == department & toppayor$aisle == aisle),]
     asort <- head(das[order(das$no_orders, decreasing= TRUE),], n = topN)
     return(asort)
   } else if(is.null(department) == TRUE & is.null(aisle) ==  TRUE )
-  {oppd <- aggregate(order_prod_prior_dept$order_id
+  {toppayor <- aggregate(order_prod_prior_dept$order_id
                      , list(order_prod_prior_dept$department
                             ,order_prod_prior_dept$aisle
                             ,order_prod_prior_dept$product_name)
                      , length)
-  names(oppd) <- c("department", "aisle", "product_name", "no_orders")
-  das <- oppd
+  names(toppayor) <- c("department", "aisle", "product_name", "no_orders")
+  das <- toppayor
   asort <- head(das[order(das$no_orders, decreasing= TRUE),], n = topN)
   return(asort)
-  } 
+  }
 }
+
+
+
+toppayor <- aggregate(Total_Amount_of_Payment_USDollars ~ Submitting_Applicable_Manufacturer_or_Applicable_GPO_Name
+                      + Physician_Specialty
+                      ,cmspayments,FUN = sum)
+colnames(toppayor) <- c("Company","TotalPayment", "Physician_Specialty")
